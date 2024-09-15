ce2bacceabf465f3e6cfa72af788f9ff167cb9d3
lerry <lvdachao@gmail.com>
2024-09-15 17:59:34 +0800

    Update bottles.tsx

--- src/pages/bottles.tsx
+++ src/pages/bottles.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Skeleton, Button, useDisclosure } from "@nextui-org/react";
 import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 import DefaultLayout from "@/layouts/default";
 import { getBottleImage } from "@/utils/storage";
