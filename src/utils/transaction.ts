30e1ffc4276c649d0f4ab37a538e5f7c2cf06bfc
yjshi <495054021@qq.com>
2024-09-17 01:16:11 +0800

    add different env packageId

--- src/utils/transaction.ts
+++ src/utils/transaction.ts
@@ -1,9 +1,10 @@
 import { Transaction } from "@mysten/sui/transactions";
 
 const CONTRACT_ADDRESS =
-  "0x791441ea7d717ed19d75b8edfc0f13256108cae4016c21e6dbf23e5d728a99dc";
+  "0xd0bb9f2fb93d5d750a859758b8c75fcb3416bfc71dc8684912de35cd030912b7";
 // 0x791441ea7d717ed19d75b8edfc0f13256108cae4016c21e6dbf23e5d728a99dc mainnet
 // 0xd0bb9f2fb93d5d750a859758b8c75fcb3416bfc71dc8684912de35cd030912b7 testnet
+// 0x80386f5abfe2fc11cf1ba5ac0be8341c685ba5cac4fd5bfaf804b1f79f79d586 devnet
 export const EventType = `${CONTRACT_ADDRESS}::social_bottle::BottleEvent`;
 const CreateBottleMethod = `${CONTRACT_ADDRESS}::social_bottle::createBottle`;
 const ReplyBottleMethod = `${CONTRACT_ADDRESS}::social_bottle::openAndReplyBottle`;
